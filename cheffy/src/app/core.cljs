5d61306b8f6cb5876579b9566912172a7568a0fd
Aaron Addleman <aaronaddleman@gmail.com>
2019-05-23 18:48:03 -0700

    Adding cheffy appfiles

--- cheffy/src/app/core.cljs
+++ cheffy/src/app/core.cljs
@@ -1,12 +1,21 @@
 (ns app.core
-  (:require [reagent.core :as r]))
+  (:require [reagent.core :as r]
+            [re-frame.core :as rf]
+            [app.db]
+            [app.nav.views.nav :refer [nav]]
+            [app.theme :refer [cheffy-theme]]
+            ["@smooth-ui/core-sc" :refer [Normalize ThemeProvider Button]]))
 
 (defn app
   []
-  [:div "Cheffy"])
+  [:<>
+   [:> Normalize]
+   [:> ThemeProvider {:theme cheffy-theme}
+    [nav]]])
 
 (defn ^:dev/after-load start
   []
+  (rf/dispatch-sync [:initialize-db])
   (r/render [app]
     (.getElementById js/document "app")))
 
