859fca23f7f129485400b64a749a9529c43fc7ff
Aaron Addleman <aaronaddleman@gmail.com>
2019-09-08 14:51:34 -0700

    Add router dispatch
    - If applied, this commit will...
    
    - Explain why this change is being made...
    
    - Provide links to any relevant tickets, articles, or other resources...

--- cheffy/src/app/nav/events.cljs
+++ cheffy/src/app/nav/events.cljs
@@ -1,5 +1,16 @@
 (ns app.nav.events
-  (:require [re-frame.core :refer [reg-event-db]]))
+  (:require [re-frame.core :refer [reg-event-db reg-fx]]
+            [app.router :as router]))
+
+(reg-fx
+ :navigate-to
+ (fn [{:keys [path]}]
+   (router/set-token! path)))
+
+(reg-event-db
+ :route-changed
+ (fn [db [_ {:keys [handler]}]]
+   (assoc-in db [:nav :active-page] handler)))
 
 (reg-event-db
  :set-active-nav
